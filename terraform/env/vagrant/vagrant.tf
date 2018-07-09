ebd846312a3bdcb07c4f29d630751942578e7997
visscher <visscher@zorgdomein.nl>
2018-07-09 15:54:26 +0200

    - rename to generic component

--- terraform/env/vagrant/vagrant.tf
+++ terraform/env/vagrant/vagrant.tf
@@ -1,12 +1,12 @@
-resource "vault_policy" "vagrant-patientconversation" {
-	name = "vagrant-patientcoversation"
+resource "vault_policy" "vagrant-testapp" {
+	name = "vagrant-testapp"
 
 	policy = <<EOT
-path "transit/environments/vagrant/encrypt/patientconversation" {
+path "transit/environments/vagrant/encrypt/testapp" {
   capabilities = ["create", "update"]
 }
 
-path "transit/environments/vagrant/decrypt/patientconversation" {
+path "transit/environments/vagrant/decrypt/testapp" {
   capabilities = ["create", "update"]
 }
 
