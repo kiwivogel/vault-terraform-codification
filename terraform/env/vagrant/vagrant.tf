3b2296c68552b84767bfc0b4da21c5d4ea81042a
visscher <visscher@zorgdomein.nl>
2018-07-09 16:05:48 +0200

    add approle backend and testapp approle

--- terraform/env/vagrant/vagrant.tf
+++ terraform/env/vagrant/vagrant.tf
@@ -1,7 +1,7 @@
 resource "vault_policy" "vagrant-testapp" {
-	name = "vagrant-testapp"
+	name     = "vagrant-testapp"
 
-	policy = <<EOT
+	policy   = <<EOT
 path "transit/environments/vagrant/encrypt/testapp" {
   capabilities = ["create", "update"]
 }
@@ -14,4 +14,19 @@ path "auth/token/renew/*" {
   policy = "write"
 }
 EOT
+}
+
+resource "vault_auth_backend" "vagrant-approle" {
+
+	type      = "approle"
+	path      = "approle/environments/vagrant"
+
+}
+
+resource "vault_approle_auth_backend_role" "vagant-testapp-role" {
+
+	backend   = "${vault_auth_backend.vagrant-approle.path}"
+	role_name = "vagrant-test-app"
+	policies  = ["vagrant-testapp"]
+
 }
